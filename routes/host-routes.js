69802d8757971eac663c20409cd04f068a2678bc
Dave Fischer <canoebrook@hotmail.com>
2018-05-23 18:05:32 -0400

    host stuff

--- routes/host-routes.js
+++ routes/host-routes.js
@@ -11,12 +11,12 @@ var moment = require('moment');
 module.exports = function (app) {
 
 app.get("/my-sessions", function(req, res){
-  console.log("my-sessions req======================");
-  console.log(req.body);
-  console.log(req.user);
-  console.log("my-sessions res======================");
-  console.log(res);
-  console.log("my-sessions end res==================");
+  // console.log("my-sessions req======================");
+  // console.log(req.body);
+  // console.log(req.user);
+  // console.log("my-sessions res======================");
+  // console.log(res);
+  // console.log("my-sessions end res==================");
 
   db.sessions.findAll({
     attributes: ['conn_info', 'name', 'item_date', 'description'],
@@ -38,36 +38,12 @@ app.get("/my-sessions", function(req, res){
     order: [
          ['item_date', 'ASC']
        ]
-  }).then(sessions => {
-      const hostSessions = sessions.map(session => {
-
-        //tidy up the session data
-
-        return Object.assign(
-          {},
-          {
-            conn_info: session.conn_info,
-            name: session.name,
-            item_date: session.item_date,
-            description : session.description,
-            attends: session.attends.map(People => {
-
-              //tidy up the post data
-              return Object.assign(
-                {},
-                {
-                  logon_id: People.logon_id,
-                  lst_nam: People.lst_nam,
-                  fst_nam: People.fst_nam
-                }
-                )
-            })
-          }
-        )
-      });
-      // res.json(resObj)
-      console.log(hostSessions);
-      res.render("hostSession", hostsessions);
+  }).then(function (sessions) {
+      console.log(sessions);
+      var data = { 
+        sessions : sessions
+      }
+      res.render("hostSession",data);
     }).catch(function(){
        res.status(500).end();
     });
